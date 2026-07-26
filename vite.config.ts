290eba6fc208067d02a6a058ca2835395b177085
Edrees Alamroni <edrees.dev@gmail.com>
2026-07-26 23:03:35 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,28 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import inertia from '@inertiajs/vite';
-import react from '@vitejs/plugin-react';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 
 export default defineConfig({
+    build: {
+        rolldownOptions: {
+            checks: {
+                pluginTimings: true,
+            },
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
             refresh: true,
         }),
         inertia(),
-        react({
-            babel: {
-                plugins: ['babel-plugin-react-compiler'],
-            },
+        react(),
+        babel({
+            presets: [reactCompilerPreset()],
         }),
         tailwindcss(),
         wayfinder({
