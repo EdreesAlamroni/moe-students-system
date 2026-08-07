9fa3e30a45c0662334f891981b60784f090de770
Edrees Alamroni <edrees.dev@gmail.com>
2026-08-07 22:39:27 +0200

    Refactor username generation logic

--- resources/js/lib/generate-username.ts
+++ resources/js/lib/generate-username.ts
@@ -1,22 +1,67 @@
-const USERNAME_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-const USERNAME_LENGTH = 8;
+const USERNAME_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const USERNAME_DIGITS = '0123456789';
+
+const USERNAME_LETTER_COUNT = 3;
+const USERNAME_DIGIT_COUNT = 5;
+
+function pickRandomCharacter(alphabet: string, randomValue: number): string {
+    return alphabet[randomValue % alphabet.length];
+}
 
 /**
- * Generate a random username of uppercase English letters and digits.
+ * Generate a random 8-character username of 3 uppercase English letters and 5 digits,
+ * randomly distributed throughout the username.
  *
  * Example: `A4K9P2X7`
  */
-export function generateUsername(length: number = USERNAME_LENGTH): string {
-    const characterCount = USERNAME_ALPHABET.length;
-    const randomValues = new Uint32Array(length);
+export function generateUsername(): string {
+    const characters: string[] = [];
+    const characterCount = USERNAME_LETTER_COUNT + USERNAME_DIGIT_COUNT;
+    const randomValues = new Uint32Array(characterCount + characterCount - 1);
 
     crypto.getRandomValues(randomValues);
 
-    let username = '';
+    let randomIndex = 0;
 
-    for (const value of randomValues) {
-        username += USERNAME_ALPHABET[value % characterCount];
+    for (let index = 0; index < USERNAME_LETTER_COUNT; index++) {
+        characters.push(pickRandomCharacter(USERNAME_LETTERS, randomValues[randomIndex++]));
     }
 
-    return username;
+    for (let index = 0; index < USERNAME_DIGIT_COUNT; index++) {
+        characters.push(pickRandomCharacter(USERNAME_DIGITS, randomValues[randomIndex++]));
+    }
+
+    for (let index = characters.length - 1; index > 0; index--) {
+        const swapIndex = randomValues[randomIndex++] % (index + 1);
+
+        [characters[index], characters[swapIndex]] = [characters[swapIndex], characters[index]];
+    }
+
+    return characters.join('');
 }
+
+
+// OLD VERSION
+// const USERNAME_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+// const USERNAME_LENGTH = 8;
+
+// /**
+//  * Generate a random username of uppercase English letters and digits.
+//  *
+//  * Example: `A4K9P2X7`
+//  */
+// export function generateUsername(length: number = USERNAME_LENGTH): string {
+//     const characterCount = USERNAME_ALPHABET.length;
+//     const randomValues = new Uint32Array(length);
+
+//     crypto.getRandomValues(randomValues);
+
+//     let username = '';
+
+//     for (const value of randomValues) {
+//         username += USERNAME_ALPHABET[value % characterCount];
+//     }
+
+//     return username;
+// }
+
